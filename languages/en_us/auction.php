351b0836fd0be374d25bd22a582c4edbbfb399fb
Chen-Fengling <245828824@qq.com>
2017-10-26 00:51:54 +0800

    language

---
+++ languages/en_us/auction.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * ECSHOP 时间JS语言文件
+ * ============================================================================
+ * ============================================================================
+ * $Author: liubo $
+ * $Id: calendar.php 17217 2011-01-19 06:29:08Z liubo $
+*/
+$_LANG['recommended_host'] = "热拍推荐";
+$_LANG['residue_time'] = "剩余时间：";
+$_LANG['offer_my'] = "我要出价";
+$_LANG['starting_price'] = "起拍价：";
+$_LANG['current_price_snatch'] = "当前价";
+$_LANG['ci'] = "次";
+
+
+
+
+//js语言项
+$_LANG['js_languages']['Service_hotline'] = '服务热线：'; 
+$_LANG['js_languages']['day'] = '天';
+$_LANG['js_languages']['hour'] = '小时';
+$_LANG['js_languages']['hour_two'] = '时';
+$_LANG['js_languages']['minute'] = '分';
+$_LANG['js_languages']['seconds'] = '秒';
+$_LANG['js_languages']['has_ended'] = '已结束！';
